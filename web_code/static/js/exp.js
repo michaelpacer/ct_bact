f5bd18aebac1baf5137abc805a62ed68ea070ed1
DanielNScott <dnscott87@gmail.com>
2015-10-13 09:10:44 -0700

    Fixed questionnaire submission issue, other tweaks.
    Unpacked task.js into...
    driver.js        - the psiturk backbone of the project
    exp.js           - the experiment code, i.e. blinking lights
    questionnaire.js - the questionnaire at the end...
    
    Modified HTML structure of experiment:
    1) Previously exp.html was getting preloaded by task.js and then
       shown using showPage() in exp.js. This was wrong, since exp.js
       is the actual wrapper for the whole experiment and what initiates
       task.js, psiturk, etc.
    2) Now stage.html is being used to hold the "actual" experiment html,
       i.e. the grid of images & the description text.

--- web_code/static/js/exp.js
+++ web_code/static/js/exp.js
@@ -1,49 +1,3 @@
-/*
- * Requires:
- *     psiturk.js
- *     utils.js
- */
-
-// Initalize psiturk object
-var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
-
-var mycondition = condition;            // these two variables are passed by the psiturk server process
-var mycounterbalance = counterbalance;  // they tell you which condition you have been assigned to
-                                        // they are not used in the stroop code but may be useful to you
-
-// All pages to be loaded after Ad page which, accepted, splashes to consent page. 
-var pages = [
-	"instruct-desc.html",
-	"instruct-ex-exp.html",
-	"instruct-ex-form.html",
-	"exp.html",
-	"postquestionnaire.html"
-];
-
-var images = ["/static/images/lit.jpg",
-			  "/static/images/normal.jpg",
-			  "/static/images/indOn.jpg",
-			  "/static/images/indOff.jpg"
-];
-
-psiTurk.preloadPages(pages);
-psiTurk.preloadImages(images);
-
-var instructionPages = ["instruct-desc.html",
-	                    "instruct-ex-exp.html",
-	                    "instruct-ex-form.html",
-	                    ];
-
-
-/********************
-* HTML manipulation
-*
-* All HTML files in the templates directory are requested 
-* from the server when the PsiTurk object is created above. We
-* need code to get those pages from the PsiTurk object and 
-* insert them into the document.
-********************/
-
 //------------------------------------------------------------//
 // Below is the ct_bact experiment.
 //
@@ -52,25 +6,23 @@ var instructionPages = ["instruct-desc.html",
 //
 //------------------------------------------------------------//
 var experiment = function() {
-	psiTurk.showPage('exp.html');
 
+	// Initialize some global vars:
 	var dsc             = new Array();
 	var eTimes          = new Array();
 	var lightOrder      = new Array();
 	var dTimesIndex     = 0;
 	var tracker			= new Array();
-
-	// Initialize some global vars:
 	var eTimesIndex     = 0;	 		// event times index
 	var lightOrderIndex = 0; 			// which light is going to be switched next?
-	var stopToggle      = 0; 			// should the experiment keep going?
 	var clock           = 0;
-	var time            = 0; 			//
-	var elapsed         = 0; 			//
-	var offsets         = new Array();	//
-	var expCounter      = 0;
-	var keepIterating   = 1;
-	var alertMsg        = ''
+	var time            = 0; 			// The actual time.
+	var elapsed         = 0; 			// The elapsed time in the current experimental condition
+	var offsets         = new Array();	// A record of timer discrepancy, for self correcting.
+	var expCounter      = 0;			// Which experimental condition are we in?
+	var keepIterating   = 1;			// Boolean, keep time stepping?
+	var alertMsg        = ''			// Dynamically changed, a description of the exp. condition.
+	var endTime			= 2             // This should be 60, but can be set low for debugging purposes...
 
 
 	if (Math.floor(2*Math.random()) == 0){ var condition = 'rad'} else { var condition = 'noRad'}
@@ -78,15 +30,15 @@ var experiment = function() {
 	Array.prototype.sum = function() {return this.reduce(function(a,b){return a+b;});}
 
 	// Set up the default experiment imagery:
-	document.getElementById('beginButton').style.display='inline';
-	for(var i=1;i<=40;i++){
-		document.getElementById('bact' + i).src="/static/images/normal.jpg";
-	}
 
-	addEvent(document.getElementById('beginButton'),'click',beginTimeStepping);	
-	addEvent(document.getElementById('nextButton' ),'click',nextPage);
+	// addEvent(document.getElementById('beginButton'),'click',beginTimeStepping);	
+	// addEvent(document.getElementById('nextButton' ),'click',nextPage);
 
 	function setupPage(){
+		for(var i=1;i<=40;i++){
+			document.getElementById('bact' + i).src="/static/images/normal.jpg";
+		}
+
 		document.getElementById('interim1'   ).style.display = 'none'
 		document.getElementById('exp'        ).style.display = 'block'
 		document.getElementById('beginButton').style.display = 'block'
@@ -156,7 +108,7 @@ var experiment = function() {
 			}
 		}
 
-		if( elapsed === 2) {
+		if( elapsed === endTime) {
 			keepIterating = 0
 			if (expCounter < 2 ) {
 				showInterim()
@@ -193,81 +145,10 @@ var experiment = function() {
 		}
 	}
 
+	psiTurk.showPage('stage.html');
+
+	$('#beginButton').click(beginTimeStepping)
+	$('#nextButton').click(nextPage)
+
 	setupPage()
 };
-
-
-
-/****************
-* Questionnaire *
-****************/
-
-var Questionnaire = function() {
-
-	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
-
-	record_responses = function() {
-
-		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
-
-		$('input').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);		
-		});
-	};
-
-	prompt_resubmit = function() {
-		replaceBody(error_message);
-		$("#resubmit").click(resubmit);
-	};
-
-	resubmit = function() {
-		replaceBody("<h1>Trying to resubmit...</h1>");
-		reprompt = setTimeout(prompt_resubmit, 10000);
-		
-		psiTurk.saveData({
-			success: function() {
-			    clearInterval(reprompt); 
-                psiTurk.computeBonus('compute_bonus', function(){finish()}); 
-			}, 
-			error: prompt_resubmit
-		});
-	};
-
-	// Load the questionnaire snippet 
-	psiTurk.showPage('postquestionnaire.html');
-	psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
-	
-	$("#submit").click(function () {
-	    record_responses();
-	    psiTurk.saveData({
-            success: function(){
-                psiTurk.computeBonus('compute_bonus', function() { 
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
-            }, // for some reason when prompt_resubmit gets triggered below, replaceBody can't be found ...
-            error: alert('error!') });//prompt_resubmit});
-	});
-    
-	
-};
-
-function addEvent(element, evnt, funct){
-  if (element.attachEvent)
-   return element.attachEvent('on'+evnt, funct);
-  else
-   return element.addEventListener(evnt, funct, false);
-}
-
-// Task object to keep track of the current phase
-var currentview;
-
-/*******************
- * Run Task
- ******************/
-$(window).load( function(){
-    psiTurk.doInstructions(
-    	instructionPages, 						       // a list of pages you want to display in function
-    	function() { currentview = new experiment(); } // what you want to do when you are done with instructions
-    );
-});
-
